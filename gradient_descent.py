4107c164595a7e3c97b3194dc1a125935922d81e
manish-83-ops <manishkumarstudy29@gmail.com>
2025-11-14 22:38:30 +0530

    added simple gradient descent code

---
+++ gradient_descent.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Data
+x = np.array([1, 2, 3], dtype=float)
+y = np.array([2, 4, 6], dtype=float)
+
+m = 0
+c = 0
+lr = 0.01
+
+for i in range(1000):
+    y_pred = m * x + c
+    m = m - lr * np.mean((y_pred - y) * x)
+    c = c - lr * np.mean(y_pred - y)
+
+print("m =", m)
+print("c =", c)
